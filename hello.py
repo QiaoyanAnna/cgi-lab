4ebf3c7797f0c2f68005805e204f45e8c857c91c
Annabelle Zhang <qiaoyan1@ualberta.ca>
2020-01-20 18:13:07 -0700

    upadted

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import cgi
+import cgitb
+import os
+cgitb.enable()
+
+print("Content-Type: text/plain\n")
+print()
+# print("<!doctype html><title>Hello</title><h2>Hello World</h2>")
+print(os.environ)
+# for name, value in sorted(os.environ.items()):
+#     print(" " + name + "=" + value)
